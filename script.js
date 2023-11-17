c734dda44fca4d5f910805bea9909890637a2d9d
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2023-11-17 20:57:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const browserInfoDiv = document.getElementById('browser-info');
+
+    // Retrieve browser information from the navigator object
+    const browserName = navigator.appName;
+    const browserVersion = navigator.appVersion;
+
+    // Display the browser information in the div
+    browserInfoDiv.innerText = "You are using " + browserName + " version " + browserVersion;
